049681ebc7c83a116cd81adb7b5c33fb452366e6
Jordi Rubió <rubiofeina@gmail.com>
2024-09-16 13:01:46 +0200

    Modal upgrades

--- src/components/components.js
+++ src/components/components.js
@@ -1,18 +1,17 @@
-
 function Components() {
-    console.log("Exec components");
-    const components = document.querySelector(".components-list");
+  console.log("Exec components");
+  const components = document.querySelector(".components-list");
 
-    //
-    // MODAL
-    //
-    const modal = document.getElementById('custom-modal');
-    const openButton = document.getElementById('open-button');
-  
-    openButton.addEventListener('click', () => {
-        console.log("Abrir modal");
-      modal.openModal();
-    });
+  //
+  // MODAL
+  //
+  const modal = document.getElementById("custom-modal");
+  const openButton = document.getElementById("open-button");
+
+  openButton.addEventListener("click", () => {
+    console.log("Abrir modal");
+    modal.openModal();
+  });
 }
 
 export default Components = new Components();
--- src/components/wc-modal.js
+++ src/components/wc-modal.js
@@ -2,7 +2,8 @@
 const template = document.createElement("template");
 template.innerHTML = `
   <style>
-    .jw-modal {
+
+    .wc-modal {
       display: none;
       position: fixed;
       top: 0;
@@ -12,15 +13,30 @@ template.innerHTML = `
       background: rgba(0, 0, 0, 0.6);
       justify-content: center;
       align-items: center;
+
+      &.open {
+        display: flex;
+      }
+
+      .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 5px 10px;
+
+      }
+
+      .body {
+        
+      }
+
+      .footer {
+
+      }
     }
 
-    .jw-modal.open {
-      display: flex;
-    }
-
-    .modal-content {
+    .modal-window {
       background-color: white;
-      padding: 20px;
       border-radius: 10px;
       position: relative;
       max-width: 500px;
@@ -28,19 +44,17 @@ template.innerHTML = `
     }
 
     .close-button {
-      position: absolute;
-      top: 10px;
-      right: 10px;
-      background: none;
-      border: none;
-      font-size: 1.5rem;
       cursor: pointer;
     }
+
   </style>
 
-  <div id="modal" class="jw-modal">
-    <div class="modal-content">
-        <slot name="header"></slot>
+  <div id="modal" class="wc-modal">
+    <div class="modal-window">
+        <div class="header">
+          <slot name="header"></slot>
+          <span class="close-button">X</span>
+        </div>
         <slot name="body"></slot>
         <slot name="footer"></slot>
     </div>
@@ -58,15 +72,11 @@ class WcModal extends HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
-  // TOODO: Lanzar evento modalCreado para poderlo llamar el open desde otro componente
-
   connectedCallback() {
     // Seleccionar el modal y el botón de cierre
     this.#modal = this.shadowRoot.querySelector("#modal");
     this.#closeButton = this.shadowRoot.querySelector(".close-button");
-
-    // Añadir event listener para cerrar el modal
-    this.#closeButton.addEventListener("click", () => this.closeModal);
+    this.#closeButton.addEventListener("click", () => this.closeModal());
 
     // Renderizar el contenido inicial (si es necesario)
     this.render();
@@ -79,18 +89,16 @@ class WcModal extends HTMLElement {
   // Método para abrir el modal
   openModal() {
     this.#modal.classList.add("open");
-    document.body.classList.add("jw-modal-open");
   }
 
   // Método para cerrar el modal
   closeModal() {
     this.#modal.classList.remove("open");
-    document.body.classList.remove("jw-modal-open");
   }
 
   disconnectedCallback() {
     // Limpiar los event listeners cuando el componente sea removido del DOM
-    this.#closeButton.removeEventListener("click", this.closeModal.bind(this));
+    this.#closeButton.removeEventListener("click", () => this.closeModal);
   }
 }
 
